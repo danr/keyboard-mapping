90ae42f790babb7c2ce2cbf1b33b1a9f1b3133ca
Dan Rosén <dan.rosen@nbis.se>
2021-02-24 18:03:51 +0100

    Replace transfer with oneshot

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## keyboard mapping
 
-Keymap inspired by callum's qmk firmware
+Keymap loosely inspired by callum's qmk firmware
 https://github.com/qmk/qmk_firmware/tree/master/users/callum
 
 Designed for the split keyboard R-go which has a split space bar.
@@ -22,10 +22,8 @@ This compiles all dependencies and starts the required processes.
 The idea is to have a shift modifier on the left thumb.
 When pressed it also enables further modifier keys around the left hand home row:
 
-                         (,)  (.)
-    row above:           trf  agr
-    home row:     shift  nav  alt  ctrl
-                   (a)   (o)  (e)  (u)
+    home row:     shift  ctrl  alt  nav  oneshot
+                   (a)   (o)   (e)  (u)   (i)
 
 **alt**, **ctrl**:
   This releases the simulated shift key and instead makes alt or ctrl pressed.
@@ -38,36 +36,25 @@ When pressed it also enables further modifier keys around the left hand home row
   This makes the right hand home row become arrow keys and the row
   below that home, pg dn, pg up and end.
 
-**agr**:
-  This is AltGR/X's Mod5. This is configured in X for example using xmodmap.
+**oneshot**:
+  This oneshot mode makes the combinations that are otherwise impossible
+  possible to create, like `ctrl a`. This is a three step process:
 
-**trf**:
-  This _transfer_ mode makes the combinations that are otherwise impossible
-  possible to create, like `ctrl a`. This _transfer_ mode moves the normal
-  left hand keys to the right hand so that its layout becomes:
-
-    f',.py
-    daoeui
-    b:qjkx
-
-So to press `ctrl a` instead press `ctrl trf h` which is `shift u , h`. As a picture:
-
-        ctrl   a        (desired)
-          |   / \
-        ctrl trf h      (use trf)
-          |   |  h
-    shift u   ,  h
+  1. Press down the modifiers you want (in the example: `ctrl`)
+  2. Now press the oneshot trigger (in the example: `i`)
+     Now the keyboard is "frozen": key up events are ignored and it is waiting for a key down event.
+  3. Press the desired key (in the example: `a`).
+     This will happen under the desired modifiers. Now everything else is released and
+     the keyboard is unfrozen and the shift modifier mode is exited.
 
 #### GUI on next key of left thumb
 
 Left thumb needs to have another key as well for the window manager.
 This modifier key has many names like `gui`, `super`, `windows key`, `leftmeta`.
-Here I repurpose left alt. In `gui` mode:
+Here I repurpose left alt. In `gui` mode these are accessible just like in shift mode:
 
-    home row:     shift  nav  alt  ctrl
-                   (a)   (o)  (e)  (u)
-
-There is no `trf` here, just configure your WM so that it does not use `aoeu`.
+    home row:     shift  ctrl  alt  nav  oneshot
+                   (a)   (o)   (e)  (u)   (i)
 
 #### Swedish mode
 
@@ -85,8 +72,9 @@ The left shift when tapped sends backspace and the gui key when tapped sends del
 Enter is next to left space (on Alt Gr).
 
 Using xmodmap:
-    - Semicolon and colon are swapped
-    - Escape and caps lock are swapped
+
+- Semicolon and colon are swapped
+- Escape and caps lock are swapped
 
 
 ### rhand.c
--- modifiers.c
+++ modifiers.c
@@ -10,6 +10,7 @@ int main(void) {
     int Shift_mode = 0;
     int Gui_mode = 0;
     int Swe_mode = 0;
+    int oneshot = 0;
 
     #define MS 128
 
@@ -29,27 +30,39 @@ int main(void) {
 
     FILE* log = stderr; //fopen("log", "a");
 
+    int Unreal[256];
+    clear(Unreal, 0);
+
     #define KEY_NAVIGATION KEY_KATAKANA
-    #define KEY_TRANSFER   KEY_HIRAGANA
-    #define KEY_SHIFTPROXY KEY_HENKAN
+    #define KEY_SHIFTPROXY KEY_HIRAGANA
     #define KEY_SHIFT      KEY_YEN
 
     revmap[KEY_NAVIGATION] = "KEY_NAVIGATION";
-    revmap[KEY_TRANSFER] = "KEY_TRANSFER";
     revmap[KEY_SHIFTPROXY] = "KEY_SHIFTPROXY";
-    revmap[KEY_SHIFT] = "KEY_SHIFT";
-
-    int Unreal[256];
-
-    clear(Unreal, 0);
+    revmap[KEY_SHIFT]      = "KEY_SHIFT";
 
     Unreal[KEY_NAVIGATION] = 1;
-    Unreal[KEY_TRANSFER] = 1;
     Unreal[KEY_SHIFTPROXY] = 1;
-    Unreal[KEY_SHIFT] = 1;
+    Unreal[KEY_SHIFT]      = 1;
 
     int unreal(int i) {
         return i >= 0 && i < 256 && Unreal[i];
+    }
+
+    void print_state(struct input_event event, char* reason) {
+        fprintf(log, "Shift_mode: %d ", Shift_mode);
+        fprintf(log, "Gui_mode: %d ", Gui_mode);
+        fprintf(log, "Swe_mode: %d ", Swe_mode);
+        fprintf(log, "oneshot: %d ", oneshot);
+        for (int i = 0; i < MS; ++i) {
+            if (Actual[i] == 0) {
+                fprintf(log, " ");
+            } else {
+                fprintf(log, "%c", revmap[i][4]);
+            }
+        }
+        fprintf(log, " %d %s %s %d\n", event.value, reason, revmap[event.code], event.code);
+        fflush(log);
     }
 
     void send(struct input_event event, char* reason) {
@@ -61,19 +74,8 @@ int main(void) {
             Actual[event.code] = event.value;
         }
 
-        if (event.type == EV_KEY && event.value > -1) {
-            fprintf(log, "Shift_mode: %d ", Shift_mode);
-            fprintf(log, "Gui_mode: %d ", Gui_mode);
-            fprintf(log, "Swe_mode: %d ", Swe_mode);
-            for (int i = 0; i < MS; ++i) {
-                if (Actual[i] == 0) {
-                    fprintf(log, " ");
-                } else {
-                    fprintf(log, "%c", revmap[i][4]);
-                }
-            }
-            fprintf(log, " %d %s %s %d\n", event.value, reason, revmap[event.code], event.code);
-            fflush(log);
+        if (event.type == EV_KEY) {
+            print_state(event, reason);
         }
     };
 
@@ -82,7 +84,18 @@ int main(void) {
     while (fread(&event, sizeof(event), 1, stdin) == 1) {
         int passthrough = 1;
 
-        if (event.type == EV_KEY) {
+        if (event.type == EV_KEY && oneshot) {
+            passthrough = 0;
+            if (event.value == 1) {
+                send(event, "oneshot    ");
+                Shift_mode = 0;
+                Gui_mode = 0;
+                oneshot = 0;
+                clear(Desired, 0);
+            } else {
+                print_state(event, "oneshot... ");
+            }
+        } else if (event.type == EV_KEY) {
             if (event.code == KEY_YEN && !Gui_mode) {
                 if (event.value == 1) {
                     Shift_mode = 1;
@@ -114,11 +127,21 @@ int main(void) {
 
             modal_map(Shift_mode, KEY_SHIFT, KEY_SHIFTPROXY);
             modal_map(Shift_mode, KEY_A, KEY_RIGHTSHIFT);
-            modal_map(Shift_mode, KEY_S, KEY_NAVIGATION);
+            modal_map(Shift_mode, KEY_S, KEY_LEFTCTRL);
             modal_map(Shift_mode, KEY_D, KEY_LEFTALT);
-            modal_map(Shift_mode, KEY_F, KEY_LEFTCTRL);
-            modal_map(Shift_mode, KEY_W, KEY_TRANSFER);
-            modal_map(Shift_mode, KEY_E, KEY_RIGHTALT);
+            modal_map(Shift_mode, KEY_F, KEY_NAVIGATION);
+            // modal_map(Shift_mode, KEY_E, KEY_RIGHTALT); // AltGr unused for now
+
+            modal_map(Gui_mode, KEY_LEFTALT, KEY_LEFTMETA);
+            modal_map(Gui_mode, KEY_A, KEY_RIGHTSHIFT);
+            modal_map(Gui_mode, KEY_S, KEY_LEFTCTRL);
+            modal_map(Gui_mode, KEY_D, KEY_LEFTALT);
+            modal_map(Gui_mode, KEY_F, KEY_NAVIGATION);
+
+            if (Shift_mode | Gui_mode && event.code == KEY_G && event.value == 1) {
+                oneshot = 1;
+                passthrough = 0;
+            }
 
             if (Shift_mode && event.code == KEY_RIGHTSHIFT) {
                 if (event.value == 1) {
@@ -126,11 +149,6 @@ int main(void) {
                 }
             }
 
-            modal_map(Gui_mode, KEY_LEFTALT, KEY_LEFTMETA);
-            modal_map(Gui_mode, KEY_A, KEY_RIGHTSHIFT);
-            modal_map(Gui_mode, KEY_S, KEY_NAVIGATION);
-            modal_map(Gui_mode, KEY_D, KEY_LEFTALT);
-            modal_map(Gui_mode, KEY_F, KEY_LEFTCTRL);
 
             if (Swe_mode) {
                 switch (event.code) {
@@ -158,17 +176,6 @@ int main(void) {
                     case KEY_COMMA: event.code = KEY_PAGEDOWN; break;
                     case KEY_DOT: event.code = KEY_PAGEUP; break;
                     case KEY_SLASH: event.code = KEY_END; break;
-                }
-            }
-
-            if (Actual[KEY_TRANSFER]) {
-                int c = event.code;
-                int r0 = c >= KEY_7 && c <= KEY_MINUS;
-                int r1 = c >= KEY_U && c <= KEY_LEFTBRACE;
-                int r2 = c >= KEY_J && c <= KEY_APOSTROPHE;
-                int r3 = c >= KEY_M && c <= KEY_RIGHTSHIFT;
-                if (r0 || r1 || r2 || r3) {
-                    event.code = c - 6;
                 }
             }
         }
