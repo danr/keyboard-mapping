b9bb0e3cabd9a4e2f58cfadba7b73d32adbc3bac
Dan Rosén <danr42@gmail.com>
2021-03-15 09:41:16 +0100

    Fix Mode_switch for kadv

---
+++ keylogger.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+
+#include "revmap.h"
+
+int main(void) {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
+    #define NUM_WATCHES 128
+
+    int State[NUM_WATCHES];
+
+    memset(State, 0, sizeof(State));
+
+    FILE* log = stderr;
+
+    struct input_event event;
+
+    while (fread(&event, sizeof(event), 1, stdin) == 1) {
+        fwrite(&event, sizeof(event), 1, stdout);
+
+        if (event.type == EV_KEY && event.code < NUM_WATCHES) {
+            State[event.code] = event.value;
+        }
+
+        if (event.type != 0) {
+            for (int i = 0; i < NUM_WATCHES; ++i) {
+                if (State[i] == 0) {
+                    fprintf(log, " ");
+                } else {
+                    fprintf(log, "%c", revmap[i][4]);
+                }
+            }
+            fprintf(log, " %3d %-20s %d %d\n", event.code, revmap[event.code], event.value, event.type);
+        }
+    }
+}
--- setup.sh
+++ setup.sh
@@ -13,17 +13,15 @@ Thinkpad=/dev/input/by-path/platform-i8042-serio-0-event-kbd
 if test -e $Thinkpad; then
     sudo intercept -g $Thinkpad |
         ./rhand |
-        ./remap KEY_CAPSLOCK  KEY_ESC      \
-                KEY_ESC       KEY_CAPSLOCK \
-                KEY_SPACE     KEY_YEN      \
-                KEY_RIGHTALT  KEY_SPACE    \
-                KEY_SYSRQ     KEY_ENTER    \
-                KEY_LEFTCTRL  KEY_KATAKANA \
-                KEY_RIGHTCTRL KEY_RIGHTALT \
+        ./remap KEY_CAPSLOCK  KEY_ESC       \
+                KEY_ESC       KEY_CAPSLOCK  \
+                KEY_SPACE     KEY_BACKSPACE \
+                KEY_RIGHTALT  KEY_SPACE     \
+                KEY_SYSRQ     KEY_ENTER     \
                 |
         dual-function-keys -c <(echo '
             MAPPINGS:
-              - KEY: KEY_YEN
+              - KEY: KEY_BACKSPACE
                 TAP: KEY_BACKSPACE
                 HOLD: KEY_LEFTCTRL
               - KEY: KEY_LEFTALT
@@ -35,8 +33,7 @@ if test -e $Thinkpad; then
               - KEY: KEY_ENTER
                 TAP: KEY_ENTER
                 HOLD: KEY_RIGHTALT
-        ') |
-        ./homerow_nav |
+        ') | # ./homerow_nav |
         sudo uinput -d $Thinkpad &
 fi
 
@@ -45,12 +42,14 @@ if test -e $Kadv; then
     sudo intercept -g $Kadv |
         ./remap KEY_CAPSLOCK   KEY_ESC        \
                 KEY_ESC        KEY_CAPSLOCK   \
-                KEY_BACKSPACE  KEY_YEN        \
                 KEY_DELETE     KEY_LEFTALT    \
+                KEY_BACKSPACE  KEY_YEN        \
+                KEY_LEFTALT    KEY_BACKSPACE  \
                 KEY_LEFTCTRL   KEY_LEFTMETA   \
                 KEY_RIGHTCTRL  KEY_RIGHTALT   \
                 KEY_LEFTBRACE  KEY_RIGHTBRACE \
                 KEY_RIGHTBRACE KEY_LEFTBRACE  \
+                KEY_RIGHTMETA  KEY_RIGHTALT   \
                 |
         dual-function-keys -c <(echo '
             MAPPINGS:
@@ -60,13 +59,8 @@ if test -e $Kadv; then
               - KEY: KEY_LEFTALT
                 TAP: KEY_DELETE
                 HOLD: KEY_LEFTALT
-        ') |
-        ./swe_and_nav | dual-function-keys -c <(echo '
-            MAPPINGS:
-              - KEY: KEY_KP0
-                TAP: KEY_Z
-                HOLD: KEY_RIGHTSHIFT
-        ') | sudo uinput -d $Kadv &
+        ') | # ./keylogger |
+        sudo uinput -d $Kadv &
 fi
 
 
@@ -75,12 +69,12 @@ sleep 0.4
 setxkbmap dvorak -option "compose:102"
 
 xmodmap <(echo '
-! mode switch on AltGr
+! mode switch on AltGr (KEY_RIGHTALT)
 clear Mod1
 clear Mod5
 keycode 108 = Mode_switch
-add Mod5 = Mode_switch
 add Mod1 = Alt_L Alt_R
+add Mod5 = Mode_switch
 ') || true
 
 xset r rate 150 45
